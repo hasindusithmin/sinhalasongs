7d420bba7aab4ad5be817addf41d05656f22ebfb
hasindusithmin <hasindusithmin64@gmail.com>
2022-10-21 08:36:18 +0530

    Create styles files

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+import '../styles/w3.css'
+import '../styles/inconsolata.css'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className='w3-light-grey w3-content' style={{maxWidth:'1600px'}}>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
